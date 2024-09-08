37b9043c9503f20a74d2fdd56e4a927e467a4a56
Nour <mohamednour25@gmail.com>
2024-09-08 19:48:16 +0300

    Initial Commit

---
+++ Arthmitc Progression/Arthmitc_ProgressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arthmitc_ProgressionApp.swift
+//  Arthmitc Progression
+//
+//  Created by administrator on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Arthmitc_ProgressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
